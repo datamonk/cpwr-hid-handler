7c40417e455cdca7bb0780c3ef59334e81429beb
afcamar <acamara86@gmail.com>
2025-10-11 23:47:49 -0400

    runtime mode arg, output obj reorder by key

--- read-ups.js
+++ read-ups.js
@@ -1,10 +1,6 @@
 const HID = require('node-hid');
 const EventEmitter = require('events');
 
-const doneEmitter = new EventEmitter(); // Event emitter to signal when parsing is done
-let upsDevice; // HID device instance
-const reportData = {}; // Init JSON obj for parsed report data
-
 /** @globals */
 // Vendor and Product IDs for the CyberPower SL950U UPS
 const vendorId = 0x0764; // Vendor ID [Cyberpower]
@@ -14,61 +10,94 @@ const productId = 0x0501; // Product ID [SL950U]
 //const productId = 0x0601; // Product ID [PR1500LCDRT2U]
 
 const devicePath = "/dev/usb/hiddev0"; // Local device pointer
-const reportsToParse = ['0x66', '0x68', '0xd0']; // Report/Usage IDs to parse
+const usagesToParse = ['0x66', '0x68', '0xd0']; // Report/Usage IDs to parse
+
+const ee = new EventEmitter(); // Event emitter to signal state of parsing lifecycle
+let ups; // HID device instance
+const reportData = {}; // Init JSON obj for parsed report data
+// Desired key order for final output
+const keyOrder = ['ts', 'path', 'batteryPercentage', 'acPresent', 'runTimeToEmpty', 'chargeStatus'];
 /***/
+
+const userArgs = process.argv.slice(2);
+// Simple arg parser for --device or -d flag to specify device path
+for (let i = 0; i < userArgs.length; i++) {
+  if (userArgs[i] === '--mode' || userArgs[i] === '-m') {
+    if (i + 1 < userArgs.length) {
+      const mode = userArgs[i + 1].toLowerCase();
+      var onceModeEnabled = false;
+      if (mode === 'once') {
+        console.warn('Setting runtime mode to once. The script will exit after a single read cycle.');
+        onceModeEnabled = true;
+      } else if (mode === 'stream') {
+        console.warn('Setting runtime mode to stream. The script will continue running and reading data.');
+        //onceModeEnabled = false;
+      } else {
+        console.error('Invalid mode. Use "once" or "stream".');
+        process.exit(1);
+      }
+    } else {
+      console.error('No mode provided after --mode or -m flag.');
+      process.exit(1);
+    }
+  } else if (userArgs[i] === '--help' || userArgs[i] === '-h') {
+    console.log('Usage: node read-ups.js [--mode <once|stream>]');
+    process.exit(0);
+  }
+}
 
 /**
  * Parses the raw HID data buffer from the UPS.
- * @param {string} reportId - The report ID in hex string format (e.g., '0x66').
+ * @param {string} usageId - The report/usage ID in hex string format (e.g., '0x66').
  * @param {buffer} data - The raw data buffer from the HID device.
- * @param {object} outputObject - The object to store parsed data.
+ * @param {object} output - The object to store parsed data.
  * @returns {object} - An object with the parsed UPS data.
  */
-function parseHidData(reportId, buffer, outputObject) {
+function parseHidData(usageId, buffer, output) {
   if (buffer.length < 5) {
     return; // Ignore short or malformed reports, seems to speed up processing..
   };
 
-  switch (reportId) {
+  switch (usageId) {
     case '0x66':
       const remainingCapacity = buffer.readUInt16LE(4); // Bytes 4-5
-      const runTimeToEmpty = buffer.readUInt16LE(8); // Bytes 8-9
 
-      outputObject.remainingCapacity = remainingCapacity;
-      outputObject.runTimeToEmpty = runTimeToEmpty;
+      output.batteryPercentage = remainingCapacity;
       break;
     case '0x68':
+      /** hut1.6 usage desc states 'Run Time to Empty' unit is in minutes..but its 
+       * actually seconds. So we need to convert it to min here.
+       * @ref: https://usb.org/sites/default/files/hut1_6.pdf#page=386&zoom=100,57,57
+       */
       const runTimeSec = buffer.readUInt16LE(4); // Bytes 4-5
-      const runTimeMin = Math.floor(runTimeSec / 60); // convert to minutes
+      const runTimeMin = Math.floor(runTimeSec / 60); // convert to min
 
-      outputObject.runTimeMin = runTimeMin;
+      output.runTimeToEmpty = runTimeMin;
       break;
     case '0xd0':
       const acPresent = (buffer[4] & 0b00000001) !== 0;
       const charging = (buffer[12] & 0b00000001) !== 0;
       const discharging = (buffer[20] & 0b00000001) !== 0;
       const fullyCharged = (buffer[28] & 0b00000001) !== 0;
-      let chargerStatus = "Undefined";
+      let chargeStatus = "undefined";
 
       if (charging && acPresent && !discharging && !fullyCharged) {
-        chargerStatus = "Charging"; // State when on AC power and charging
+        chargeStatus = "charging"; // State when on AC power and charging
       } else if (discharging && !charging && !fullyCharged) {
-        chargerStatus = "Discharging"; // State when on battery and discharging
+        chargeStatus = "discharging"; // State when on battery and discharging
       } else if (fullyCharged && acPresent && !charging && !discharging) {
-        chargerStatus = "Fully Charged"; // State when on AC power and fully charged
-      } else if (!acPresent && !charging && !discharging && !fullyCharged) {
-        chargerStatus = "On Battery"; // State when on battery and not charging
+        chargeStatus = "fully-charged"; // State when on AC power and fully charged
       };
-      outputObject.acPresent = acPresent;
-      outputObject.chargerStatus = chargerStatus;
+      output.acPresent = acPresent;
+      output.chargeStatus = chargeStatus;
 
-      /** Via synchronous read(s), this is the last report id we are going to 
-       * parse data from so add a timestamp to the output object and signal 
-       * our emitter that we are done. This will ensure we have the final 
-       * complete data object before returning it downstream.
+      /** Since this is a synchronous read, we can lean safely assume this is the last id
+       * we are going to parse data from. Add timestamp, devicePath to the output object
+       * and signal a done state to the emitter. Ensuring a complete data object is gtg.
        */
-      outputObject.timestamp = new Date().toISOString();
-      doneEmitter.emit('done');
+      output.ts = new Date().toISOString();
+      output.path = devicePath;
+      ee.emit('done');
       break;
     default: break; // Skip all other ids
   };
@@ -78,50 +107,66 @@ function parseHidData(reportId, buffer, outputObject) {
  * Kickoff the HID event handler for reading from the UPS device.
  */
 function startUpsHandler() {
-  console.log('Starting UPS HID handler...');
+  //console.log('Starting UPS HID handler...');
   try {
-    upsDevice = new HID.HID(devicePath);
-    console.log(`Opened device: ${devicePath}`);
-    console.log('Listening for data... Press Ctrl+C to exit.');
+    ups = new HID.HID(devicePath);
+    //console.log(`Opened device: ${devicePath}`);
+    //console.log('Listening for data... Press Ctrl+C to exit.');
 
-    upsDevice.on('data', function (data) {
+    ups.on('data', function (data) {
       const reportId = data[0];
       const reportIdHex = `0x${reportId.toString(16).padStart(2, '0')}`;
 
-      if (reportsToParse.includes(reportIdHex)) {
+      if (usagesToParse.includes(reportIdHex)) {
         parseHidData(reportIdHex, data, reportData);
         if (reportIdHex === '0xd0') {
-          doneEmitter.once('done', () => {
-            console.log('Final JSON object:', JSON.stringify(reportData, null, 2));
-            
-            // @todo: Decide to hold the connection open or close it after a single 
-            //        read cycle.
-            upsDevice.close();
-            console.log('Closed device after single read cycle.');
-
-            // Reset reportData for next read cycle
-            //for (const key in reportData) {
-            //  if (reportData.hasOwnProperty(key)) {
-            //    delete reportData[key];
-            //  };
-            //};
-
+          ee.once('done', () => {
+            const entries = Object.entries(reportData);
+            entries.sort((a, b) => {
+              const indexA = keyOrder.indexOf(a[0]);
+              const indexB = keyOrder.indexOf(b[0]);
+              return indexA - indexB;
+            });
+            const orderedReportData = Object.fromEntries(entries);
+            console.log(JSON.stringify(orderedReportData, null, 2));
+            /**
+             * @output:
+             * {
+                 "ts": "2025-10-12T03:19:11.546Z",
+                 "path": "/dev/usb/hiddev0",
+                 "batteryPercentage": 100,
+                 "acPresent": true,
+                 "runTimeToEmpty": 42,
+                 "chargeStatus": "fully-charged"
+               }
+             */
+            if (onceModeEnabled) {
+              ups.close();
+              //console.log('Closed device after single read cycle.');
+            } else {
+              // Reset reportData for next read cycle
+              for (const key in reportData) {
+                if (reportData.hasOwnProperty(key)) {
+                  delete reportData[key];
+                };
+              };
+            };
           });
         };
       };
     });
 
-    upsDevice.on('error', function (err) {
+    ups.on('error', function (err) {
       console.error('HID device error:', err);
-      if (upsDevice) {
-        upsDevice.close();
+      if (ups) {
+        ups.close();
       };
       setTimeout(startUpsHandler, 5000);
     });
 
     process.on('SIGINT', () => {
       console.log('\nClosing device and exiting.');
-      upsDevice.close();
+      ups.close();
       process.exit();
     });
 
