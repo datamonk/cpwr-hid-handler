2bd9039db1258368de6921748d646a5c602557c5
afcamar <acamara86@gmail.com>
2025-10-13 23:15:51 -0400

    bin exec mods so its easier to run.. i think

--- bin/read-ups.sh
+++ bin/read-ups.sh
@@ -2,25 +2,25 @@
 
 set -o pipefail; ##
 # @file: read-ups.sh
-# @tldr: Wrapper bash script to exec a nested nodejs file
-#        bundled in the repo.
+# @tldr: Wrapper bash script to exec nodejs file [read-ups.js]
+#        and acting as the primary module binary.
 ##
 
 ## @globals
-_wai(){ printf "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"; };
-_wai="$(_wai)"; _pn="${0##*/}";
+_wai(){ printf "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"; }; _wai="$(_wai)"; 
+# Strip the extension from this procname and use it for the js exec
+_pn="${0##*/}"; _scr="$(basename "${_pn}.js")";
 ##
 
-exec_njs(){
-  # Only grab filename from the first arg to reset path context 
-  # from this bin directory.
-  local scr; scr="$(basename "$1")"; shift;
-  if [[ ! -f "${scr}" ]]; then
-    echo "Error: JavaScript file '${scr}' not found."; return 1;
+main(){
+  if [[ ! -f "${_scr}" ]]; then
+    echo "Error: JavaScript file '${_scr}' not found.";
+    return 1;
   fi
-  $(type -p node) "${_wai}/../${scr}" "$@"; return $?;
+  $(type -p node) "${_wai}/../${_scr}" "$@";
+  return $?;
 };
-# @usage: exec_njs "./read-ups.js" "--mode" "once" "--verbose"
+# @usage: ./bin/read-ups --mode once --verbose
 # @output:
 #   ----- Running [read-ups.js] at Mon 13 Oct 2025 11:28:36 AM EDT -----
 #
@@ -40,8 +40,8 @@ exec_njs(){
 #   [VERBOSE] Closed device after single read cycle.
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  echo -e "----- Running [$(basename "$1")] at $(date) -----\n";
-  exec_njs "$@";
+  echo -e "----- Running [${_scr}] at $(date) -----\n";
+  main "$@";
   # shellcheck disable=SC2181
   if [[ $? -ne 0 ]]; then
     echo "Error processing arguments."; exit 1;
